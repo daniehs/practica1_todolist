aff26806d817e8ef9e8196c70ab674536ebd5b54
Daniel Hernandez <daniel.hernandez@pucp.edu.pe>
2020-07-09 20:08:24 -0500

    Variables globales con variables fuera

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-let global='';
+let color = "";
+let indice = 1;
+
 const click_handler = (e) => {
   let itarea = document.querySelector(".input_tarea");
   let contenedor_tarea = document.querySelector("#tareas");
@@ -6,27 +8,25 @@ const click_handler = (e) => {
 
   switch (e.target.classList.value) {
     case "prioridad_max":
-      global='mariana';
-      console.log(global);
-      contenedor_tarea.classList.value = "";
-      contenedor_tarea.classList.add("prioridad_max");
+      color = "rojo";
+      // contenedor_tarea.classList.value = "";
+      // contenedor_tarea.classList.add("prioridad_max");
       e.preventDefault();
 
       break;
 
     case "prioridad_med":
-      global='wendy';
-      console.log(global);
-
-      contenedor_tarea.classList.value = "";
-      contenedor_tarea.classList.add("prioridad_med");
+      color = "amarillo";
+      // contenedor_tarea.classList.value = "";
+      // contenedor_tarea.classList.add("prioridad_med");
       e.preventDefault();
 
       break;
 
     case "prioridad_min":
-      contenedor_tarea.classList.value = "";
-      contenedor_tarea.classList.add("prioridad_min");
+      color = "verde";
+      // contenedor_tarea.classList.value = "";
+      // contenedor_tarea.classList.add("prioridad_min");
       e.preventDefault();
 
       break;
@@ -34,31 +34,24 @@ const click_handler = (e) => {
     case "agregar_tarea": {
       e.preventDefault();
 
-      if (
-        itarea.value &&
-        fingresada.value &&
-        contenedor_tarea.classList.value &&
-        new Date(fingresada.value) - Date.now() > 0
-      ) {
-
-    
+      let fecha = new Date(fingresada.value) > Date.now();
+      console.log(new Date(fingresada.value),Date.now());
+      if (itarea.value && fingresada.value && color && fecha) {
         //Creamos un div que sera la tarjeta de la tarea
+        console.log(indice);
         let divtarea = document.createElement("DIV");
         //Este es el numero de tarjetas 0,1,2....childelementcount
-        let ntareas=document.getElementById("tareas").childElementCount;
-         (ntareas>0)?
-         ntareas=document.getElementById("tareas").lastElementChild.classList[0].substring(6)*1+1:
-         ntareas=1;
+        let ntareas = document.getElementById("tareas").childElementCount;
+
+        ntareas > 0 ? indice++ : indice > 1 ? indice++ : indice;
+
         //Colocamos una clase para colocar a cada tarjeta de trabajo que se agregue "tarea-0, tarea-1,..."
-        divtarea.classList.add(
-          `tarea-${ntareas}`,
-          contenedor_tarea.classList.value
-        );
+        divtarea.classList.add(`tarea-${indice}`, color);
         let litarea = document.createElement("LI");
         litarea.textContent = itarea.value;
 
         let temp = document.createElement("SPAN");
-        temp.classList.add(`temporizador-${ntareas}`);
+        temp.classList.add(`temporizador-${indice}`);
         let spanb = document.createElement("SPAN");
         let icon = document.createElement("I");
         icon.classList.add("fas");
@@ -68,26 +61,24 @@ const click_handler = (e) => {
 
         contenedor_tarea.appendChild(divtarea);
         //aqui agrego la clase que debo tomar en cuenta
-        let tarea = document.querySelector(`.tarea-${ntareas}`);
+        let tarea = document.querySelector(`.tarea-${indice}`);
         tarea.appendChild(litarea);
         tarea.appendChild(temp);
         tarea.appendChild(spanb);
 
-        let temporizador = document.querySelector(`.temporizador-${ntareas}`);
+        let temporizador = document.querySelector(`.temporizador-${indice}`);
 
-        timer(fingresada.value, temporizador);
+        timer(fingresada.value, temporizador, indice);
 
         itarea.value = "";
         fingresada.value = "";
       } else {
         let error = document.createElement("P");
         let merror;
-        if (!contenedor_tarea.classList.value)
-          merror = "Elija un color de prioridad para la tarea";
+        if (!color) merror = "Elija un color de prioridad para la tarea";
         if (!fingresada.value) merror = "Debe ingresar una fecha";
         if (!itarea.value) merror = "Nombre de tarea es obligatorio";
-        if (new Date(fingresada.value) - Date.now() < 0)
-          merror = "Debe elegir una fecha mayor a la actual ";
+        if (!fecha) merror = "Debe elegir una fecha mayor a la actual ";
 
         error.textContent = merror;
         console.log(error);
@@ -109,25 +100,19 @@ document.querySelector(".wrapper").addEventListener("click", click_handler);
 
 anadircero = (hora) => (hora < 10 ? "0" + hora : hora);
 
-indice=(lastchild)=>{
-
-// let i = lastchild.
-}
 const eliminar = (e) => {
   // console.log(e.target.parentElement.parentElement);
-  console.log(global);
-  if (e.target.classList.contains('fa-backspace')) {
-    console.log('entro');
+  if (e.target.classList.contains("fa-backspace")) {
+    console.log("entro");
     document
       .querySelector("#tareas")
       .removeChild(e.target.parentElement.parentElement);
-     clearInterval(timer);
   }
 };
 document.querySelector("#tareas").addEventListener("click", eliminar);
 
-const timer = (fechasel, display) => {
-  console.log(fechasel, display);
+const timer = (fechasel, display, indice) => {
+  console.log(fechasel, display, indice);
   setInterval(() => {
     let difsegundos = Math.abs(new Date(fechasel) - Date.now()) / 1000;
 
